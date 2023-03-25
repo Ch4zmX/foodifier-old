dc954dc002be5a018d2c0a8f0d2b6a77e73372a0
Jay Tripathi <jaytripathi100@gmail.com>
2023-03-25 02:13:26 -0700

    single meal parser works now, changed to local htm source cause website is down over break

--- menu.py
+++ menu.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import time
 import urls
-
+import re
 
 def get_meal_from_time(time):
     pass
@@ -27,26 +27,41 @@ def get_site_with_cookie(url, location_url):
 
 
 def get_meal(college, meal="Auto", day="Today"):  # get single meal, defaults to auto based on current time
+    
     food_items = {}
     div = ''
     if meal == "Auto":
-        print("Auto feature not set! Manually choose a meal\nSetting meal to lunch...")
+        print("Auto feature not set! Manually choose a meal!\nSetting meal to lunch...")
         meal = "Lunch"
     location_url = urls.LOCATION_URLS[college]
 
     full_url = urls.BASE_URL+location_url+urls.MEAL_URL+meal
-
     print(full_url)
+    
     # longmenucolmenucat - divider names
     # longmenucoldispname - menu items name
-    response = get_site_with_cookie(full_url, location_url) # get html of specified menu
-    soup = BeautifulSoup(response.content, 'lxml')
-    table = soup.find('div', {'class': 'longmenuinstructs'}).parent.find_all('div')[1].find('table') #tbody containing trs for each menu item/divider
-    for tr in table.find_all('td'):
+    with open("menu.htm", "r") as f:
+        response = bytes(f.read(), 'utf-8')  #get_site_with_cookie(full_url, location_url) # get html of specified menu
 
-        print(tr)
-    #print(table.prettify())
+    soup = BeautifulSoup(response, 'lxml')
+    table = soup.find('div', {'class': 'longmenuinstructs'}).parent.find_all('div')[1].find('tbody') #tbody containing trs for each menu item/divider
 
+        
+    for tr in table.find_all('tr',recursive=False):
+        if (divider := tr.find('div',{'class':'longmenucolmenucat'})) is not None:
+            #print(divider.text)
+            continue
+        if (food := tr.find('div', {'class':'longmenucoldispname'})) is not None:
+            food_items[food.text] = []
+            for img in tr.find_all('img'):
+                diets = img['src'].split('/')[1].split('.')[0]
+                food_items[food.text].append(diets)
+                
+        else:
+            continue
+
+    print(food_items)
+    print(full_url)
 
 
 '''
