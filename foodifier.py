fa304e005dd476e6c2703ebf303e6aba72f7641d
Jay Tripathi <jaytripathi100@gmail.com>
2023-03-26 15:04:08 -0700

    bot can now respond with menu, need to test with more dhs

---
+++ foodifier.py
@@ -0,0 +1,45 @@
+import discord
+import datetime
+from menu import *
+from urls import *
+
+if __name__ == '__main__':
+    WHITELIST = ['bot-stuff'] # bot will only run if message is in whitelisted channel
+    MESSAGE = 'cowell'
+
+    with open('token.txt', 'r') as f: # if running this yourself, create a bot token and put it in token.txt (purpose is so i can share this program without sharing my token)
+        TOKEN = f.read().strip()
+
+    intents = discord.Intents.default()
+    intents.message_content = True
+    
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        print(f'Client has logged in as {client.user}')
+
+    @client.event
+    async def on_message(message):
+        if str(message.channel) not in WHITELIST:
+            return
+        if MESSAGE in str(message.content):
+            meal = await get_meal('cowell/stevenson', 'Lunch')
+
+            message_str = ''
+            for food in meal.keys():
+                if food == '-- Cereal --':
+                    break
+                #print(food)
+                
+                if food in DIVIDERS:
+                    message_str += f'**{food.replace("-","").strip()}**\n'
+                    continue
+                message_str += food + ' '
+                for diet_restriction in meal[food]:
+                    #print(diet_restriction)
+                    message_str += ' ' + EMOJIS[diet_restriction] + ' ' # add emojis to output string representing dietary restrictions
+                message_str += '\n'
+            #print(message_str)
+            await message.channel.send(message_str) 
+    client.run(TOKEN)
--- urls.py
+++ urls.py
@@ -9,4 +9,7 @@ LOCATION_URLS = {
 MEAL_URL = "&WeeksMenus=UCSC+-+This+Week%27s+Menus&mealName="
 
 MEALS = ['Breakfast', 'Lunch', 'Dinner', 'Late Night', 'Auto']  # Auto meal selects meal based on current time
-
+DIVIDERS = ['-- Grill --', '-- Clean Plate --', '-- DH Baked --', '-- Bakery --', '-- Open Bars --', '-- Cereal --', '-- All Day --', '-- Condiments --', 
+            '-- Salad Bar --', '-- Deli Bar --', '-- Bread and Bagels --', '-- Beverages --'] # strings corresponding to the dividers, will be used to determine menu validity 
+                                                                                      # (eg if cereal is first divider found, then the dh is not open for that meal)
+EMOJIS = {'veggie':'🥦', 'vegan':'🌱', 'halal':'🍖', 'eggs':'🥚', 'beef':'🐮', 'milk':'🥛', 'fish':'🐟', 'alcohol':'🍷', 'gluten':'🍞', 'soy':'🇸'}
