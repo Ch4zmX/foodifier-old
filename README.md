43a18cba4cb7a012d0da8ace725089c8b5eb08f6
Jay Tripathi <jaytripathi100@gmail.com>
2023-03-31 20:05:45 -0700

    add comments

--- README.md
+++ README.md
@@ -6,4 +6,9 @@ Discord bot to notify of UCSC dining hall menu items
 - Show nutritional facts + nutritional calculator
     - Add a database for nutrition facts (Whenever new item's calorie count is requested it will take the value and add it to database, shouldn't change)
 - Auto select meal based on current time and schedule
-    - Specified meal does not exist if first "recipe description" is cereal (itll still show the menu, even for clown on weekends)
+    - Specified meal does not exist if first "recipe description" is cereal (itll still show the menu, even for crown on weekends)
+- Add notify command
+    - Args
+        - food_item: string of food item to notify of 
+- /blacklist command
+    - location: dining hall location to not get notified from 
--- foodifier.py
+++ foodifier.py
@@ -1,14 +1,14 @@
+from datetime import datetime, timedelta
+from typing import *
+
 import discord
 from discord.ext import commands
-from typing import *
-from datetime import datetime
-from datetime import timedelta  
+
 from menu import *
 from urls import *
 
-
 if __name__ == '__main__':
-    WHITELIST = ['bot-stuff', 'foodifier'] # bot will only run if message is in whitelisted channel
+    WHITELIST = ['bot-stuff', 'foodifier', 'menu'] # bot will only run if message is in whitelisted channel (these are specific to my test server)
     INTENTS = discord.Intents.all()
     INTENTS.message_content = True
     with open('token.txt', 'r') as f: # if running this yourself, create a bot token and put it in token.txt (purpose is so i can share this program without sharing my token)
@@ -30,63 +30,63 @@ if __name__ == '__main__':
                    meal: Literal[tuple(MEALS)],  # Autocomplete choices for meal parameter
                    day_offset: Literal[tuple([i for i in range(0,19)])] = 0): # max offset is tested to be 18 
         user, channel, command = str(interaction.user), str(interaction.channel), str(interaction.command)
-        if channel not in WHITELIST:
+        if channel not in WHITELIST: # only accept commands in whitelisted channels
             print(f'Ignoring command in {channel} channel')
             return
         
-        if location not in LOCATION_URLS.keys():
+        if location not in LOCATION_URLS.keys(): # error if incorrect location entered
             print(f'Invalid dining location specified: "{location}"')
             menu_embed = discord.Embed(title=f'Invalid dining location specified: "{location}"\n', color=0xFF0000)
             await interaction.response.send_message(embed=menu_embed)
             return
-        if meal not in MEALS:
+        if meal not in MEALS: # if invalid meal entered, auto set to lunch. I will later add auto meal selection based on current time and schedule of specified dh
             print(f'Invalid meal specified location specified: "{location}". Setting meal to lunch...')
             #menu_embed = discord.Embed(title=f'Invalid meal specified: "{location}"\n', color=0xFF0000)
             #await interaction.response.send_message(embed=menu_embed)
             meal = "Lunch"
         if meal == "Auto":
             print("Auto feature not set! Manually choose a meal!\nSetting meal to lunch...")
-            meal = "Lunch"    
+            meal = "Lunch"
         
         print(f'{datetime.now()}: {user} used command') # print who used what command for logging purposes
 
-        date = (datetime.now() + timedelta(days=day_offset)).strftime('%m/%d/%y') # get date of , default offset of 0
+        date = (datetime.now() + timedelta(days=day_offset)).strftime('%m/%d/%y') # get menu parsing date from current date and offset with a default offset of 0 for today
         
         try:
             meal_dict = get_meal(location, meal, date) # try except in case command 
         except Exception as e:
             print(f'Failed fetching menu: {str(e)}')
-            menu_embed = discord.Embed(title=f'Failed fetching specified menu: "{location}"\n', color=0xFF0000)
+            menu_embed = discord.Embed(title=f'Failed fetching specified menu: "{location}"\n', color=0xFF0000) # error message if exception occured
             await interaction.response.send_message(embed=menu_embed)
             return
         if meal_dict is None: # if menu was not found the value will be None
             print("Bot response: meal unavailable")
-            menu_embed = discord.Embed(title=f'{meal} at {location} on {date} is unavailable!\n', color=0xFF0000)
+            menu_embed = discord.Embed(title=f'{meal} at {location} on {date} is unavailable!\n', color=0xFF0000) # error message if meal not available
             await interaction.response.send_message(embed=menu_embed)
             return
         
-        message_str = ''
-        for food in meal_dict.keys():
+        message_str = '' # empty message to add to
+        for food in meal_dict.keys(): 
             food = food.strip()
-            if food == '-- Cereal --':
+            if food == '-- Cereal --': # cereal divider means end of actual menu
                 break
             
-            if food in DIVIDERS:
+            if food in DIVIDERS: # ignore dividers and show as bold
                 message_str += f'**{food.replace("-","").strip()}**\n'
                 continue
 
-            message_str += food + ' '
-            if meal_dict[food] is not None:
+            message_str += food + ' ' 
+            if meal_dict[food] is not None: # iterate through allergens/dietary restrictions and add them to each menu item
                     for diet_restriction in meal_dict[food]:
                         try:
                             message_str += ' ' + EMOJIS[diet_restriction] + ' ' # add emojis to output string representing dietary restrictions
                         except Exception as e:
-                            print(f'Error handling allergen keyword: {str(e)}')
+                            print(f'Error handling allergen keyword: {str(e)}') # if unknown allergen encountered show error so i can add it
             message_str += '\n'
 
-        menu_embed = discord.Embed(title=f'Menu for {meal} at {location} on {date}', description=message_str, color=0x50c878)
+        menu_embed = discord.Embed(title=f'Menu for {meal} at {location} on {date}', description=message_str, color=0x50c878) #send embed with green color to signify success
 
-        await interaction.response.send_message(embed=menu_embed)
+        await interaction.response.send_message(embed=menu_embed) 
 
     @bot.tree.command(name='notify', description='Get pinged when a certain meal or food item is in the menu')
     async def notify(interaction: discord.Interaction, food_item: str): # doesnt do anything yet
